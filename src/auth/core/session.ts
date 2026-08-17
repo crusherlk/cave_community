f87dbde84fec2b52b99ae7ff9ce07a335ab70e87
Pankaja Chalaru <42083827+crusherlk@users.noreply.github.com>
2026-08-17 13:28:26 +0530

    rolling session implementation

--- src/auth/core/session.ts
+++ src/auth/core/session.ts
@@ -11,13 +11,14 @@ import {
 
 export const SESSION_COOKIE = "__Host-session";
 export const SESSION_EXPIRE_SECONDS = 60 * 60 * 24 * 14; //14days
+export const SESSION_REFRESH_SECONDS = 60 * 60 * 24 * 7; //7days
 
 export async function createUserSession(userId: number) {
   const sessionId = crypto.randomBytes(512).toString("hex").normalize();
 
   await db.insert(SessionTable).values({
     token: sessionId,
-    expiresAt: new Date(Date.now() + SESSION_EXPIRE_SECONDS),
+    expiresAt: new Date(Date.now() + SESSION_EXPIRE_SECONDS * 1000),
     userId,
   });
 
@@ -31,23 +32,66 @@ async function _getUserSession() {
   const session = await findSessionByToken(sessionId);
   if (session == null) return null;
 
-  return session;
+  // check expiration & update
+  const now_ms = Date.now();
+  const remaining_ms = session.expiresAt.getTime() - now_ms;
+
+  // logic to not refresh
+  if (remaining_ms / 1000 > SESSION_REFRESH_SECONDS) {
+    return session;
+  }
+
+  // refresh session
+  const updatedSession = await updateSessionByToken(sessionId);
+  return updatedSession;
 }
 
 async function findSessionByToken(sessionId: string) {
   return await db.query.SessionTable.findFirst({
     where: {
       token: sessionId,
+      expiresAt: {
+        gt: new Date(),
+      },
     },
     with: {
       user: {
         columns: {
           id: true,
           name: true,
+          email: true,
         },
       },
     },
   });
+}
+
+async function updateSessionByToken(sessionId: string) {
+  const updatedSession = await db.transaction(async (tx) => {
+    await tx
+      .update(SessionTable)
+      .set({ expiresAt: new Date(Date.now() + SESSION_EXPIRE_SECONDS * 1000) })
+      .where(eq(SessionTable.token, sessionId));
+
+    return await tx.query.SessionTable.findFirst({
+      where: {
+        token: sessionId,
+      },
+      with: {
+        user: {
+          columns: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+  });
+  if (updatedSession == null) return null;
+
+  setSessionCookie(updatedSession.token);
+  return updatedSession;
 }
 
 export const getUserSession = cache(_getUserSession);
