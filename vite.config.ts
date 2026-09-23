233de9ae1f8f8101b6db92ca10d76e114bb9ab95
crusherlk <42083827+crusherlk@users.noreply.github.com>
2026-09-23 11:06:59 +0530

    cloudflare setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
@@ -8,6 +9,7 @@ import { defineConfig } from "vite";
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tailwindcss(),
